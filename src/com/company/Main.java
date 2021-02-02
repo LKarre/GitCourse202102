11b103fe8ebe9ef26b2cf51063b2b9ffe0bd6445
LKarre <liselotte.karre@hm.com>
2021-02-02 13:35:05 +0100

    First main change

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import javax.xml.crypto.Data;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.SortedMap;
+import java.util.SortedSet;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<String> movieList = List.of("Rick and morty","South Park","The Mandalorian","Breaking Bad", "Alternated Carbon");
+
+        System.out.println(movieList);
+
+    }
+
+
+
+
+    /*private static String findByLenght(List<String> movielist, String str){
+        String result=null;
+        for (int)*/
+    }
+
